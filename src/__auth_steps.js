3abf4483f44deb49ead7b5ee1c130feb3bdf8213
Jaman-M <jaman.manik1@gmail.comm>
2022-04-10 19:18:08 +0600

    initial firebase setup

---
+++ src/__auth_steps.js
@@ -0,0 +1,11 @@
+/**
+ * 1. create a new project in console.firebase.google.com
+ * 2. npm install
+ * 3. create firebase.init.js and import getAuth to export auth
+ * firebase settings> Authentication> enable email and password auth
+ */
+
+/**
+ * hosting steps
+ * 1. npm install -g firebase-tools
+ */
