9337ef85fdd844bffee82d183d3662442554cb8a
Jaman-M <jaman.manik1@gmail.comm>
2022-04-13 22:42:06 +0600

    style signUp component and access email, password

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -2,7 +2,9 @@
  * 1. create a new project in console.firebase.google.com
  * 2. npm install
  * 3. create firebase.init.js and import getAuth to export auth
- * firebase settings> Authentication> enable email and password auth
+ * 4. firebase settings> Authentication> enable email and password auth
+ * 5. create login, signup componetn, setup route
+ * 6. attach form field handler and form submit handler
  */
 
 /**
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,14 +7,14 @@ const Login = () => {
         <div className='form-container'>
             <div>
             <h2 className='form-title'>Login</h2>
-            <form action="">
+            <form>
             <div className="input-group">
                 <label htmlFor='email'>Emai</label>
-                <input type="email" name='email' id='' />
+                <input type="email" name='email' id='' required />
             </div>
             <div className="input-group">
             <label htmlFor='password'>Password</label>
-                <input type="password" name="password" id="" />
+                <input type="password" name="password" id="" required />
             </div>
                 <input className='form-submit' type="submit" value='Login' />
             </form>
